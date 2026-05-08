8d44efd6109ca4d97b4c55d706c7d0f49037d151
SUDHANSHU DUBEY <manudubey249@gmail.com>
2026-05-08 12:09:58 +0530

    d

--- src/app/store/slices/configApi.js
+++ src/app/store/slices/configApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const configApi = createApi({
   reducerPath: 'configApi',
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL || 'http://localhost:9999/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL}),
   endpoints: (builder) => ({
     getPlatformConfig: builder.query({
       query: () => '/config',
